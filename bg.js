94bb1aadaef3627def9c1b9f3cd5fdc9fb69740d
a <106607480+ariamaranai@users.noreply.github.com>
2026-09-20 00:35:17 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,14 +1,14 @@
 {
   let matchUrl;
-  let downloads = chrome.downloads;
-  chrome.declarativeNetRequest.onRuleMatchedDebug.addListener(e =>
+  let { downloads, declarativeNetRequest, tabs } = chrome;
+  declarativeNetRequest.onRuleMatchedDebug.addListener(e =>
     matchUrl = e.rule.ruleId === 2 && e.request.url
   );
   downloads.onCreated.addListener(({ url, id }) =>
-    url == matchUrl && (
+    url === matchUrl && (
       downloads.cancel(id),
       downloads.erase({ id }),
-      chrome.tabs.create({ url: url + "#.pdf" })
+      tabs.create({ url: url + "#.pdf" })
     )
   );
 }
